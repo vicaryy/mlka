98848c6bacc44afe954b06d358870f302f1f933e
vicary <vicarycholewa@gmail.com>
2024-06-18 20:18:29 +0200

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "test",
+    component: TestComponent
+  }
+];
