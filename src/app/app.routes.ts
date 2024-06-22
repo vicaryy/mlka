7b7c1497c9ec4cdb8c821363ada3e87f0c7385cc
vicary <vicarycholewa@gmail.com>
2024-06-22 13:22:55 +0200

    add privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,39 +7,44 @@ import { NewsComponent } from './news/news.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
-  {
-    path: "",
-    component: HomeComponent,
-    title: "MLKA · Strona Główna"
-  },
-  {
-    path: "czyste-powietrze",
-    title: "MLKA · Czyste powietrze",
-    loadComponent: () => import('./clean-air/clean-air.component').then(e => e.CleanAirComponent)
-  },
-  {
-    path: "o-nas",
-    component: AboutComponent,
-    title: "MLKA · O nas"
-  },
-  {
-    path: "realizacje",
-    component: RealizationsComponent,
-    title: "MLKA · Realizacje"
-  },
-  {
-    path: "aktualnosci",
-    component: NewsComponent,
-    title: "MLKA · Aktualności"
-  },
-  {
-    path: "kontakt",
-    component: ContactComponent,
-    title: "MLKA · Kontakt"
-  },
-  {
-    path: "**",
-    title: "MLKA · 404 - Nie znaleziono",
-    loadComponent: () => import('./not-found/not-found.component').then(e => e.NotFoundComponent)
-  }
+    {
+        path: "",
+        component: HomeComponent,
+        title: "MLKA · Strona Główna"
+    },
+    {
+        path: "czyste-powietrze",
+        title: "MLKA · Czyste Powietrze",
+        loadComponent: () => import('./clean-air/clean-air.component').then(e => e.CleanAirComponent)
+    },
+    {
+        path: "o-nas",
+        component: AboutComponent,
+        title: "MLKA · O Nas"
+    },
+    {
+        path: "realizacje",
+        component: RealizationsComponent,
+        title: "MLKA · Realizacje"
+    },
+    {
+        path: "aktualnosci",
+        component: NewsComponent,
+        title: "MLKA · Aktualności"
+    },
+    {
+        path: "kontakt",
+        component: ContactComponent,
+        title: "MLKA · Kontakt"
+    },
+    {
+        path: "polityka-prywatnosci",
+        title: "MLKA · Polityka Prywatności",
+        loadComponent: () => import('./privacy-policy/privacy-policy.component').then(e => e.PrivacyPolicyComponent)
+    },
+    {
+        path: "**",
+        title: "MLKA · 404 - Nie znaleziono",
+        loadComponent: () => import('./not-found/not-found.component').then(e => e.NotFoundComponent)
+    }
 ];
